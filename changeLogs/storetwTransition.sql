0c16bd019d17098e844c4e6aa765e868f36e1e5f
David Steinich <drsteini@contractor.usgs.gov>
2019-07-23 12:17:17 -0500

    add - if not exists

--- changeLogs/storetwTransition.sql
+++ changeLogs/storetwTransition.sql
@@ -1,4 +1,4 @@
-create table ${STORETW_SCHEMA_NAME}.storetw_transition
+create table if not exists ${STORETW_SCHEMA_NAME}.storetw_transition
 (org_id     character varying (255)
 ,date_added date
 );
